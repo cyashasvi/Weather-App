d95d2b87a6537d7b0044bf5ee1250dee318254aa
cyashasvi <cyashasvi@gmail.com>
2021-03-17 19:27:13 -0700

    got the name of city and weather information to show up

--- javascript.js
+++ javascript.js
@@ -1,19 +1,24 @@
-$( function() {
-    var cache = {};
-    $( "#birds" ).autocomplete({
-      minLength: 2,
-      source: function( request, response ) {
-        var term = request.term;
-        if ( term in cache ) {
-          response( cache[ term ] );
-          return;
-        }
- 
-        $.getJSON( "search.php", request, function( data, status, xhr ) {
-          cache[ term ] = data;
-          response( data );
-        });
-      }
-    });
-  } );
+  
+  function getApi() {
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=bbb2c52d2a0a1b8c27b92512eb107417'
+    fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+        var city = document.getElementById('city_name');
+        var temp = document.getElementById('temperature');
+        var humid = document.getElementById('humidity');
+        var wind_speed = document.getElementById('wind_speed');
+        var uv_in = document.getElementById('uv_in');
+        city.textContent = data.name;
+        temp.textContent = data.main.temp;
+        humid.textContent = data.main.humidity;
+        wind_speed.textContent = data.wind.speed;
+        uv_in.textContent = data.wind.deg;
+      console.log(city);
+    })
+  }
 
+  getApi();
