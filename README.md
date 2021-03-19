022569bb70b47a009ab9474a73fab6e8a49fc0bc
cyashasvi <cyashasvi@gmail.com>
2021-03-19 00:00:51 -0700

    made final changes before submission

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-App
+This app's functionality is to show someone a five day forecast of any city they are interested in, and then show them
+the five day forecast showing the weather, humidity, and the UV index. 
+To build this app, I had to:
+1. Create elements such as:
+    a. cards to hold the forecasts
+    b. jumbotron to show today's temperature, humidity, wind speed, and UV Index.
+    c. a search bar with a button to actually run the search.
+    d. a small card to hold the value of the city you just typed.
+2. Run two Fetch functions to bring in the OpenWeather API to pull data.
+3. Dynamically generate data onto the page from API.
+4. Move city into localstorage. 
+
+Although this app didn't have all the functionalities described in my assignment, I was still able to get the application
+to work for the most part. 
--- javascript.js
+++ javascript.js
@@ -17,12 +17,12 @@
         var temp = document.getElementById('temperature');
         var humid = document.getElementById('humidity');
         var wind_speed = document.getElementById('wind_speed');
-        var uv_in = document.getElementById('uv_in');
+       
         city.textContent = data.name;
         temp.textContent = 'It is ' + data.main.temp + ' degrees Farenheit outside.';
-        humid.textContent = data.main.humidity;
-        wind_speed.textContent = data.wind.speed;
-        uv_in.textContent = data.wind.deg;
+        humid.textContent = 'Current Humidity : ' + data.main.humidity + '%';
+        wind_speed.textContent = 'Wind Speed: ' + data.wind.speed + 'mph';
+        
       getTwo(long,lat)
     })
   }
@@ -34,7 +34,58 @@ function getTwo(long,lat) {
   })
   .then(function(data) {
     console.log(data);
-  })
+    var uv_in = document.getElementById('uv_in');
+    var dayOne = document.getElementById('dayone');
+    var dayTwo = document.getElementById('daytwo');
+    var dayThree = document.getElementById('daythree');
+    var dayFour = document.getElementById('dayfour');
+    var dayFive = document.getElementById('dayfive');
+    uv_in.textContent = 'UV Index: ' + data.daily[0].uvi;
+    dayOne.textContent = 'Tomorrow: ' + data.daily[0].temp.day + ' F'; 
+    dayTwo.textContent = 'Day After: ' + data.daily[1].temp.day + ' F'; 
+    dayThree.textContent = 'Two Days Out: ' + data.daily[2].temp.day + ' F'; 
+    dayFour.textContent = 'Three Days Out: ' + data.daily[3].temp.day + ' F'; 
+    dayFive.textContent = 'Four Days Out: ' + data.daily[4].temp.day + ' F';
+
+    if ( 0 <= data.daily[0].uvi <= 2) {
+     uv_in.style.color = 'green';
+     uv_in.style.fontWeight = 'bold'; 
+    }
+    else if ( 3 <= data.daily[0].uvi <= 5) {
+      uv_in.style.color = 'yellow';
+      uv_in.style.fontWeight = 'bold';
+    }
+    else if ( 6 <= data.daily[0].uvi <= 7) {
+      uv_in.style.color = 'orange';
+      uv_in.style.fontWeight = 'bold';
+    }
+    else if ( 8 <= data.daily[0].uvi <= 10) {
+      uv_in.style.color = 'red';
+      uv_in.style.fontWeight = 'bold';
+    }
+
+    var bodyOne = document.getElementById('bodyone');
+    var bodyTwo = document.getElementById('bodytwo');
+    var bodyThree = document.getElementById('bodythree');
+    var bodyFour = document.getElementById('bodyfour');
+    var bodyFive = document.getElementById('bodyfive');
+    bodyOne.textContent = 'UV Index: ' + data.daily[0].uvi + ' Humidity: ' + data.daily[0].humidity + '%';
+    bodyTwo.textContent = 'UV Index: ' + data.daily[1].uvi + ' Humidity: ' + data.daily[1].humidity + '%';
+    bodyThree.textContent = 'UV Index: ' + data.daily[2].uvi + ' Humidity: ' + data.daily[2].humidity + '%'; 
+    bodyFour.textContent = 'UV Index: ' + data.daily[3].uvi + ' Humidity: ' + data.daily[3].humidity + '%';
+    bodyFive.textContent = 'UV Index: ' + data.daily[4].uvi + ' Humidity: ' + data.daily[4].humidity + '%';
+
+    var bodyOneB = document.getElementById('bodyoneB');
+    var bodyTwoB = document.getElementById('bodytwoB');
+    var bodyThreeB = document.getElementById('bodythreeB');
+    var bodyFourB = document.getElementById('bodyfourB');
+    var bodyFiveB = document.getElementById('bodyfiveB');
+    bodyOneB.textContent = ' Humidity: ' + data.daily[0].humidity + '%';
+    bodyTwoB.textContent = ' Humidity: ' + data.daily[1].humidity + '%';
+    bodyThreeB.textContent = ' Humidity: ' + data.daily[2].humidity + '%'; 
+    bodyFourB.textContent = ' Humidity: ' + data.daily[3].humidity + '%';
+    bodyFiveB.textContent = ' Humidity: ' + data.daily[4].humidity + '%';
+  });
 }
   document.getElementById('search').addEventListener('click',function(){
     var citySearch = document.querySelector('#city_search').value;
